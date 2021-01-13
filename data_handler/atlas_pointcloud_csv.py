357750accd7d82398520a133c37c8a65c4b4eb68
Taylor Childers <taylor@jtchilders.com>
2021-01-13 20:52:49 +0000

    remove print statements; add two paths for training/testing so augmentation only happens on training

--- data_handler/atlas_pointcloud_csv.py
+++ data_handler/atlas_pointcloud_csv.py
@@ -27,13 +27,13 @@ def get_datasets(config):
    gnum_points = gconfig['data']['num_points']
    gnum_features = gconfig['data']['num_features']
 
-   train = from_filelist(config['data']['train_filelist'],config)
-   valid = from_filelist(config['data']['test_filelist'],config)
+   train = from_filelist(config['data']['train_filelist'],config,True)
+   valid = from_filelist(config['data']['test_filelist'],config,False)
    
    return train,valid
 
 
-def from_filelist(filelist_filename,config):
+def from_filelist(filelist_filename,config,training=False):
    logger.debug(f'build dataset {filelist_filename}')
    dc = config['data']
 
@@ -67,19 +67,14 @@ def from_filelist(filelist_filename,config):
 
    # map to read files in parallel
    logger.debug('starting map')
-   ds = filelist.map(load_csv_file,
-                     num_parallel_calls=tf.data.experimental.AUTOTUNE)  # dc['num_parallel_readers']) #
-   # ds = tf.data.experimental.CsvDataset(filelist,
-   #                                      record_defaults=[tf.int64,tf.int32,tf.float32,tf.float32,
-   #                                                       tf.float32,tf.float32,tf.float32,tf.float32,
-   #                                                       tf.float32,tf.int32,tf.int32,tf.float32,
-   #                                                       tf.float32,tf.float32],
-   #                                      field_delim='\t',
-   #                                      select_cols=[0,2,3,4,5,6,7,8,9])
+   if training:
+      ds = filelist.map(load_csv_file_training,
+                     num_parallel_calls=tf.data.experimental.AUTOTUNE)
+   else:
+      ds = filelist.map(load_csv_file_testing,
+                     num_parallel_calls=tf.data.experimental.AUTOTUNE)
    # how many inputs to prefetch to improve pipeline performance
-   ds = ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)  # dc['prefectch_buffer_size']) #
-
-   # ds = ds.apply(tf.data.Dataset.unbatch)
+   ds = ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
    # batch the data
    ds = ds.batch(dc['batch_size'],drop_remainder=True)
@@ -87,11 +82,23 @@ def from_filelist(filelist_filename,config):
    return ds
 
 
-def load_csv_file(filename):
-   return tf.py_function(load_csv_file_py,[filename],[tf.float32,tf.int32,tf.int8,tf.int8])
+def load_csv_file_training(filename):
+   return tf.py_function(load_csv_file_py_training,[filename],[tf.float32,tf.int32,tf.int8,tf.int8])
 
 
-def load_csv_file_py(filename):
+def load_csv_file_testing(filename):
+   return tf.py_function(load_csv_file_py_testing,[filename],[tf.float32,tf.int32,tf.int8,tf.int8])
+
+
+def load_csv_file_py_training(filename):
+   return load_csv_file_py(filename,training=True)
+
+
+def load_csv_file_py_testing(filename):
+   return load_csv_file_py(filename,training=False)
+
+
+def load_csv_file_py(filename,training=False):
    filename = bytes.decode(filename.numpy())
 
    df = pd.read_csv(filename,header=None,names=col_names, dtype=col_dtype, sep='\t')
@@ -100,14 +107,12 @@ def load_csv_file_py(filename):
    if len(df) > gnum_points:
       df = df[0:gnum_points]
 
-
-   if gconfig['data']['augment']:
+   if gconfig['data']['augment'] and training:
       rotation_angle,rotation_matrix = random_rotation()
       df[['x','y','z']] = np.dot(df[['x','y','z']],rotation_matrix)
-      df['phi'] = df['phi'] + (rotation_angle - np.pi)
-      df['phi'] = df['phi'].apply(lambda x: x if x < np.pi else x - np.pi)
+      # df['phi'] = df['phi'] + (rotation_angle - np.pi)
+      # df['phi'] = df['phi'].apply(lambda x: x if x < np.pi else x - np.pi)
 
-   # logger.info('1 df_inputs: %s',df_inputs.shape)
    # normalize variables
    if False:
       # build the model inputs
@@ -127,9 +132,6 @@ def load_csv_file_py(filename):
 
       # build the model inputs
       df_inputs = df[include_cols].to_numpy()
-   # logger.info('2 df_inputs: %s',df_inputs.shape)
-   # tf.print('df_inputs: ',df_inputs[0:10,...])
-
 
    # stuff ragged event sizes into fixed size
    inputs = np.zeros([gnum_points,gnum_features])
@@ -140,7 +142,6 @@ def load_csv_file_py(filename):
    df_labels = df[['pid']]
    # map pid to class label
    df_labels = df_labels.replace({'pid':labels_dict})
-   # logger.info('5 df_labels: %s %s',df_labels.shape,np.unique(df_labels,return_counts=True))
 
    # convert to numpy
    df_labels = df_labels.to_numpy()
@@ -152,25 +153,17 @@ def load_csv_file_py(filename):
    unique_classes,unique_counts = np.unique(df_labels,return_counts=True)
    # get mininum class occurance count
    min_class_count = np.min(unique_counts)
-   # logger.info('66 min_class_count: %s',min_class_count)
-   # tf.print('min_class_count:',min_class_count,unique_classes,unique_counts)
    # create class weights to be applied to loss as mask
    # this will balance the loss function across classes
    class_weights = np.zeros([gnum_points],dtype=np.int8)
    # set weights to one for an equal number of classes
    for class_label in unique_classes:
-      # tf.print('class_label:',class_label)
       class_indices = np.nonzero(df_labels == class_label)[0]
-      # logger.info('class_indices: %s %s',class_label,df_labels[class_indices])
       class_indices = np.random.choice(class_indices,size=[min_class_count],replace=False)
       class_weights[class_indices] = 1
-      # tf.print('3: ',np.unique(class_weights,return_counts=True))
-   
-   # logger.info('7 class_weights: %s %s %s',class_weights.shape,class_weights.sum(),1)
 
    nonzero_mask = np.zeros([gnum_points],dtype=np.int8)
    nonzero_mask[0:df_labels.shape[0]] = 1
-   # logger.info('8 nonzero_mask: %s %s',nonzero_mask.shape,nonzero_mask.sum())
 
    # pad with zeros or clip some points
    labels = np.zeros([gconfig['data']['num_points']])
